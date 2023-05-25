6eecb1987c6efc8d0028201a66c19472b1a4e1b1
jihadzeidane <134439176+jihadzeidane@users.noreply.github.com>
2023-05-25 13:53:26 +0300

    Update concurrent.c

--- concurrent.c
+++ concurrent.c
@@ -4,147 +4,79 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include<getopt.h>
-#include<stdbool.h>
-#include<dirent.h>
+#include <dirent.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <pthread.h>
 
-#define defaultPort 0000
+#define DEFAULT_PORT 0000
 #define BUFFER_SIZE 1024
-bool authenticateUser(const char *username, const char *password, const char *passwordFile) {
-    FILE *file = fopen(passwordFile, "r");
+
+struct ThreadArgs {
+    int client_socket;
+    const char* inventory;
+    const char* passwordUsername;
+};
+
+int authenticateUser(const char* username, const char* password, const char* passwordFile) {
+    FILE* file = fopen(passwordFile, "r");
     if (file == NULL) {
         perror("Failed to open password file");
         exit(EXIT_FAILURE);
     }
 
-    char line[100];
-    char *storedUsername;
-    char *storedPassword;
+    char line[BUFFER_SIZE];
+    while (fgets(line, BUFFER_SIZE, file)) {
+        line[strcspn(line, "\n")] = '\0';
+        char* storedUsername = strtok(line, ":");
+        char* storedPassword = strtok(NULL, ":");
 
-    while (fgets(line, sizeof(line), file)) {
-        storedUsername = strtok(line, ":");
-        storedPassword = strtok(NULL, ":");
-        storedPassword[strcspn(storedPassword, "\n")] = '\0';
-        if (storedUsername != NULL && storedPassword != NULL &&
-            strcmp(username, storedUsername) == 0 && strcmp(password, storedPassword) == 0) {
+        if (strcmp(username, storedUsername) == 0 && strcmp(password, storedPassword) == 0) {
             fclose(file);
-            return true;
+            return 1;
         }
     }
 
     fclose(file);
-    return false;
+    return 0;
 }
 
-int main(int argc,char *argv[]) {
-    int server_fd, client_socket, read_size;
-    int PORT=defaultPort;
-    char *inventory;
-    char *passwordUsername;
-    struct sockaddr_in server_addr, client_addr;
+void* clientThread(void* arg) {
+    struct ThreadArgs* threadArgs = (struct ThreadArgs*)arg;
+    int client_socket = threadArgs->client_socket;
+    const char* inventory = threadArgs->inventory;
+    const char* passwordUsername = threadArgs->passwordUsername;
+
     char buffer[BUFFER_SIZE] = {0};
-    int opt;
-    int portarg= 0;
-   while ((opt = getopt(argc, argv, "d:p:u:")) != -1) {
-        switch (opt) {
-             case 'd':
-            inventory=optarg;
+    bool accessDenied = false;
+    char username[BUFFER_SIZE];
+    char password[BUFFER_SIZE];
+
+    while (!accessDenied) {
+        write(client_socket, "Please enter your username: ", strlen("Please enter your username: "));
+        memset(buffer, 0, BUFFER_SIZE);
+        int read_size = read(client_socket, buffer, BUFFER_SIZE);
+        buffer[strcspn(buffer, "\n")] = '\0';
+        strcpy(username, buffer);
+
+        write(client_socket, "Please enter your password: ", strlen("Please enter your password: "));
+        memset(buffer, 0, BUFFER_SIZE);
+        read_size = read(client_socket, buffer, BUFFER_SIZE);
+        buffer[strcspn(buffer, "\n")] = '\0';
+        strcpy(password, buffer);
+
+        int loginResult = authenticateUser(username, password, passwordUsername);
+        if (loginResult == 1) {
+            write(client_socket, "Login successful!\n", strlen("Login successful!\n"));
             break;
-            case 'p':
-            PORT = atoi(optarg);
-            break;
-           
-            case 'u':
-             passwordUsername=optarg;
-                break;
-            default:
-            fprintf(stderr, "%s [-p port]\n", argv[0]);
-            exit(EXIT_FAILURE);
+        } else {
+            write(client_socket, "Access denied!\n", strlen("Access denied!\n"));
+            write(client_socket, "Please try again.\n", strlen("Please try again.\n"));
+            accessDenied = true;
         }
     }
 
-   
-    if(passwordUsername==NULL||inventory==NULL||PORT==defaultPort){
-        fprintf(stderr,"Arguements required to run.\n");
-        fprintf(stderr,"Pass port using [-p port]\nPass directory using [-d directory]\nPass password using [-u password]\n",argv[0]);
-        exit(EXIT_FAILURE);
-    }
-   
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket creation failed");
-        exit(EXIT_FAILURE);
-    }
-
-    
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-
-   
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
-        perror("Binding failed");
-        exit(EXIT_FAILURE);
-    }
-
-    
-    if (listen(server_fd, 3) < 0) {
-        perror("Listen failed");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server listening on port %d...\n", PORT);
-
-    while (1) {
-        int client_addr_len = sizeof(client_addr);
-
-        if ((client_socket = accept(server_fd, (struct sockaddr*)&client_addr, (socklen_t*)&client_addr_len)) < 0) {
-            perror("Accept failed");
-            exit(EXIT_FAILURE);
-        }
-        char client_ip[INET_ADDRSTRLEN];
-        if (inet_ntop(AF_INET, &(client_addr.sin_addr), client_ip, INET_ADDRSTRLEN) == NULL) {
-            perror("Inet_ntop failed");
-            exit(EXIT_FAILURE);
-        }
-
-        printf("New client connected: %s:%d\n", client_ip, ntohs(client_addr.sin_port));
-
-        bool loginSuccessful = false;
-        while (!loginSuccessful) {
-            bool accessDenied = false;
-
-            write(client_socket, "Please enter your username: ", strlen("Please enter your username: "));
-            memset(buffer, 0, BUFFER_SIZE);
-            read_size = read(client_socket, buffer, BUFFER_SIZE);
-            buffer[strcspn(buffer, "\n")] = '\0';
-            char* username = strdup(buffer);
-
-            write(client_socket, "Please enter your password: ", strlen("Please enter your password: "));
-            memset(buffer, 0, BUFFER_SIZE);
-            read_size = read(client_socket, buffer, BUFFER_SIZE);
-            buffer[strcspn(buffer, "\n")] = '\0'; 
-            char* password = strdup(buffer);
-
-          
-            loginSuccessful = authenticateUser(username, password, passwordUsername);
-            if (loginSuccessful) {
-                write(client_socket, "Login successful!\n", strlen("Login successful!\n"));
-               
-            } else {
-                write(client_socket, "Access denied!\n", strlen("Access denied!\n"));
-                write(client_socket, "Please try again.\n", strlen("Please try again.\n"));
-                accessDenied = true;
-            }
-
-            
-            free(username);
-            free(password);
-
-            if (accessDenied) {
-                continue; 
-            }
-        }
-if (loginSuccessful) {
+    if (!accessDenied) {
         write(client_socket, "Welcome to the server!\n", strlen("Welcome to the server!\n"));
         write(client_socket, "You are now logged in.\n", strlen("You are now logged in.\n"));
         write(client_socket, "You can use the following commands:\n", strlen("You can use the following commands:\n"));
@@ -153,14 +85,12 @@ if (loginSuccessful) {
 
         bool loggedIn = true;
         while (loggedIn) {
-            write(client_socket, "Please enter a command: ", strlen("Please enter a command: "));
+            write(client_socket, "Please enter a command: \n", strlen("Please enter a command: \n"));
             memset(buffer, 0, BUFFER_SIZE);
-            read_size = read(client_socket, buffer, BUFFER_SIZE);
-            buffer[strcspn(buffer, "\n")] = '\0'; 
+            int read_size = read(client_socket, buffer, BUFFER_SIZE);
+            buffer[strcspn(buffer, "\n")] = '\0';
 
-           
             if (strcmp(buffer, "list") == 0) {
-                
                 DIR* directory;
                 struct dirent* file;
 
@@ -168,37 +98,110 @@ if (loginSuccessful) {
                 if (directory == NULL) {
                     write(client_socket, "Failed to open directory.\n", strlen("Failed to open directory.\n"));
                 } else {
-                 
                     while ((file = readdir(directory)) != NULL) {
                         if (strcmp(file->d_name, ".") != 0 && strcmp(file->d_name, "..") != 0) {
-                           
                             char fileDetails[BUFFER_SIZE];
                             sprintf(fileDetails, "%s %ld\n", file->d_name, file->d_reclen);
                             write(client_socket, fileDetails, strlen(fileDetails));
                         }
                     }
                     closedir(directory);
-
-                    
                     write(client_socket, ".\n", strlen(".\n"));
                 }
             } else if (strcmp(buffer, "quit") == 0) {
-             
                 write(client_socket, "Goodbye!\n", strlen("Goodbye!\n"));
                 loggedIn = false;
             } else {
-              
                 write(client_socket, "Unrecognized command. Please try again.\n", strlen("Unrecognized command. Please try again.\n"));
             }
         }
     }
 
-    // ...
+    close(client_socket);
+    free(threadArgs);
+    pthread_exit(NULL);
 }
 
-        //user commands
+int main(int argc, char* argv[]) {
+    int server_fd, client_socket;
+    int PORT = DEFAULT_PORT;
+    char* inventory = NULL;
+    char* passwordUsername = NULL;
+    struct sockaddr_in server_addr, client_addr;
+    int opt;
+    int portarg = 0;
+    while ((opt = getopt(argc, argv, "d:p:u:")) != -1) {
+        switch (opt) {
+            case 'd':
+                inventory = optarg;
+                break;
+            case 'p':
+                PORT = atoi(optarg);
+                break;
+            case 'u':
+                passwordUsername = optarg;
+                break;
+            default:
+                fprintf(stderr, "%s [-p port]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
 
-        close(client_socket);
+    if (passwordUsername == NULL || inventory == NULL || PORT == DEFAULT_PORT) {
+        fprintf(stderr, "Arguments required to run.\n");
+        fprintf(stderr, "Pass port using [-p port]\nPass directory using [-d directory]\nPass password using [-u password]\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Binding failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Echo server listening on port %d...\n", PORT);
+
+    while (1) {
+        int client_addr_len = sizeof(client_addr);
+
+        if ((client_socket = accept(server_fd, (struct sockaddr*)&client_addr, (socklen_t*)&client_addr_len)) < 0) {
+            perror("Accept failed");
+            exit(EXIT_FAILURE);
+        }
+
+        char client_ip[INET_ADDRSTRLEN];
+        if (inet_ntop(AF_INET, &(client_addr.sin_addr), client_ip, INET_ADDRSTRLEN) == NULL) {
+            perror("Inet_ntop failed");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("New client connected: %s:%d\n", client_ip, ntohs(client_addr.sin_port));
+
+        struct ThreadArgs* threadArgs = (struct ThreadArgs*)malloc(sizeof(struct ThreadArgs));
+        threadArgs->client_socket = client_socket;
+        threadArgs->inventory = inventory;
+        threadArgs->passwordUsername = passwordUsername;
+
+        pthread_t thread;
+        if (pthread_create(&thread, NULL, clientThread, (void*)threadArgs) != 0) {
+            perror("Thread creation failed");
+            exit(EXIT_FAILURE);
+        }
+
+        pthread_detach(thread);
     }
 
     close(server_fd);
